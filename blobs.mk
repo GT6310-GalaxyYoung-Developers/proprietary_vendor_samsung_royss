a2a114e17bf83908e2e57c1ad463b9b6e9f4e72b
mamutos <pepedziobak@hotmail.com>
2014-03-07 20:51:16 +0100

    Vendor GSM Update - Part 1

--- blobs.mk
+++ blobs.mk
@@ -17,4 +17,11 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/royss/proprietary/lib/hw/sensors.default.so:/system/lib/hw/sensors.default.so \
     vendor/samsung/royss/proprietary/lib/libacdapi_azi.so:/system/lib/libacdapi_azi.so \
     vendor/samsung/royss/proprietary/lib/liboemcamera.so:/system/lib/liboemcamera.so \
-    vendor/samsung/royss/proprietary/lib/libwvm.so:/system/lib/libwvm.so
+    vendor/samsung/royss/proprietary/lib/libril-qc-qmi-1.so:/system/lib/libril-qc-qmi-1.so \
+    vendor/samsung/royss/proprietary/lib/libreference-ril.so:/system/lib/libreference-ril.so \
+    vendor/samsung/royss/proprietary/lib/libril-qcril-hook-oem.so:/system/lib/libril-qcril-hook-oem.so \
+    vendor/samsung/royss/proprietary/lib/libqcci_legacy.so:/system/lib/libqcci_legacy.so \
+    vendor/samsung/royss/proprietary/lib/libqmi_client_qmux.so:/system/lib/libqmi_client_qmux.so \
+    vendor/samsung/royss/proprietary/lib/libdsi_netctrl.so:/system/lib/libdsi_netctrl.so \
+    vendor/samsung/royss/proprietary/lib/libqdi.so:/system/lib/libqdi.so \
+    vendor/samsung/royss/proprietary/lib/libnetmgr.so:/system/lib/libnetmgr.so
